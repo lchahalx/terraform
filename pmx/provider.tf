9c2d8511a4b52521164e766ad2f0cb6d255bba5b
panjabtocan <51linuxuser@gmail.com>
2025-04-28 23:54:32 -0400

    generic

--- pmx/provider.tf
+++ pmx/provider.tf
@@ -10,8 +10,8 @@ terraform {
 
 
 provider "proxmox" {
-    pm_api_url = "http://192.168.2.165:8006/api2/json"
-    pm_api_token_id      = "lchahal@pam!terraform"
+    pm_api_url = "http://>proxmox_server>:8006/api2/json"
+    pm_api_token_id      = "<token>"
     pm_api_token_secret  = var.pm_api_token_secret
     pm_tls_insecure      = true
 }
